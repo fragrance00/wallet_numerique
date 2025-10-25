79254bab126ce3ca3761ed85b83de7c676ff656e
ABIR AKHIR <154742177+fragrance00@users.noreply.github.com>
2025-10-25 11:13:24 +0200

    SecurityConfig

---
+++ src/main/java/com/wallet/backend/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.wallet.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // désactive CSRF
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()  // autorise toutes les requêtes
+                );
+        return http.build();
+    }
+}
